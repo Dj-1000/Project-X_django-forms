90fc2f3bfa4ef56a984fa765959921b3cabf0ebe
Dheeraj Sharma <dheeraj.sharma@writlabs.com>
2024-03-04 12:42:30 +0530

    updated settings

--- crm/settings.py
+++ crm/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = "crm.wsgi.application"
 #     }}
 
 DATABASEs = {
-    "default": dj_database_url.parse(os.envrion.get('DATABASE_URL'))
+    "default": dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
